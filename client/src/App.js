35144a841dbecbc4a14dcd383500a7726881893e
KevinTamCS <kevin@kevintam.dev>
2020-04-26 01:05:33 -0400

    Split project into client and server

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,19 @@ import './assets/scss/index.scss';
 import validators from './common/validators';
 import Routes from './Routes';
 
+import * as firebase from 'firebase';
+
+const app = firebase.initializeApp({
+  apiKey: "AIzaSyD3AaaD7a6jwfw16p3LpcRoGKXnX6-osec",
+  authDomain: "hack-dsc-acb6a.firebaseapp.com",
+  databaseURL: "https://hack-dsc-acb6a.firebaseio.com",
+  projectId: "hack-dsc-acb6a",
+  storageBucket: "hack-dsc-acb6a.appspot.com",
+  messagingSenderId: "569528574190",
+  appId: "1:569528574190:web:c9d1fe3a096069d8b4d3ff",
+  measurementId: "G-GMRH4R6BPH"
+});
+
 const browserHistory = createBrowserHistory();
 
 Chart.helpers.extend(Chart.elements.Rectangle.prototype, {
